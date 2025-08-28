00b42a8502fe54ef9eeefa3885831bec02b7f044
Mateando <achimatera@hotmail.com>
2025-08-28 20:18:32 -0300

    chore: remove test script set_admin_password.php

--- scripts/set_admin_password.php
+++
@@ -1,6 +0,0 @@
-<?php
-$db=new PDO('sqlite:'.__DIR__.'/../database/database.sqlite');
-$hash=password_hash('secret123', PASSWORD_BCRYPT);
-$st=$db->prepare('UPDATE users SET password=? WHERE email=?');
-$st->execute([$hash,'admin@consultorio.com']);
-echo "updated admin password\n";
